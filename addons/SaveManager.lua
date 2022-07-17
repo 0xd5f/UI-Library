f387e6de63b29a2ae41cd3989682ce8f48ce8c04
Alex <79174725+0xd5f@users.noreply.github.com>
2022-07-17 22:56:18 +0400

    Update SaveManager.lua

--- addons/SaveManager.lua
+++ addons/SaveManager.lua
@@ -1,7 +1,7 @@
 local httpService = game:GetService('HttpService')
 
-local SaveManager = {} do
-	SaveManager.Folder = 'LinoriaLibSettings'
+local SaveManager = {} do 
+	SaveManager.Folder = 'BCWARESettings'
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
